1a90a1b18412235dad3e3d630ed780bd02c8eed5
Shayan Pakrad <shaayanpakrad@gmail.com>
2024-12-21 07:39:13 +0330

    add Service interface

---
+++ Novin.Warehouse.Backend.API/Interfaces/IService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Novin.Warehouse.Backend.API.Entities.Base;
+
+namespace Novin.Warehouse.Backend.API.Interfaces
+{
+    public interface IService<TEntity, TEntityDto, TAddUpdateDto> where TEntity : Thing
+    {
+        Task<IEnumerable<TEntityDto>> ListAsync();
+        Task<int> AddAsync(TAddUpdateDto entity);
+        Task<int> UpdateAsync(TAddUpdateDto entity);
+        Task<int> RemoveAsync(string guid);
+    }
+}
