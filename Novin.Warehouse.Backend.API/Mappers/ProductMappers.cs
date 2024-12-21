e2090ba4e82754720e00740f6b37b83cfc9a3727
Shayan Pakrad <shaayanpakrad@gmail.com>
2024-12-21 07:34:38 +0330

    add Products Mappers

---
+++ Novin.Warehouse.Backend.API/Mappers/ProductMappers.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Threading.Tasks;
+using Novin.Warehouse.Backend.API.DTOs.Products;
+using Novin.Warehouse.Backend.API.Entities;
+
+namespace Novin.Warehouse.Backend.API.Mappers
+{
+    public static class ProductMappers
+    {
+        public static ProductDto ToProductDto(this Product productModel)
+        {
+            return new ProductDto
+            {
+                Guid = productModel.Guid,
+                Name = productModel.Name,
+                Category = productModel.Category,
+                Description = productModel.Description,
+                MinQuantity = productModel.MinQuantity
+            };
+        }
+
+        public static Product ToProductFromProductDto(this ProductAddOrUpdateDto productDto, int CategoryId)
+        {
+            return new Product
+            {
+                CategoryId = CategoryId,
+                Description = productDto.Description,
+                MinQuantity = productDto.MinQuantity,
+                Name = productDto.Name
+            };
+        }
+    }
+}
