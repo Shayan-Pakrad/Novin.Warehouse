f0ed3256e39e8032c2b0ec3d9bb083aff05b7dae
Shayan Pakrad <shaayanpakrad@gmail.com>
2024-12-21 15:39:05 +0330

    add BaseApiController

---
+++ Novin.Warehouse.Backend.API/Controllers/Base/BaseApiController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Novin.Warehouse.Backend.API.Entities.Base;
+using Novin.Warehouse.Backend.API.Interfaces;
+
+namespace Novin.Warehouse.Backend.API.Controllers.Base
+{
+    // [ApiController]
+    // [Route("api/[controller]")]
+    public class BaseApiController<TService, TEntity, TEntityDto, TAddUpdateDto> : ControllerBase
+        where TService : IService<TEntity, TEntityDto, TAddUpdateDto>
+        where TEntity : Thing
+    {
+        protected readonly TService _service;
+
+        public BaseApiController(TService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("list")]
+        public virtual async Task<IResult> ListAsync()
+        {
+            return Results.Ok(await _service.ListAsync());
+        }
+
+        [HttpPost("add")]
+        public virtual async Task<int> AddAsync(TAddUpdateDto entity)
+        {
+            return await _service.AddAsync(entity);
+        }
+
+        [HttpPut("update/{guid}")]
+        public virtual async Task<int> UpdateAsync(TAddUpdateDto entity, string guid)
+        {
+            return await _service.UpdateAsync(guid, entity);
+        }
+
+        [HttpDelete("remove/{guid}")]
+        public virtual async Task<int> RemoveAsync(string guid)
+        {
+            return await _service.RemoveAsync(guid);
+        }
+    }
+}
