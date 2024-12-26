b66379e1475b04370eaeb8a1dc166f7aca546737
Shayan Pakrad <shaayanpakrad@gmail.com>
2024-12-26 17:11:26 +0330

    add InventoryDto

---
+++ Novin.Warehouse.Backend.API/DTOs/Inventories/InventoryDto.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Novin.Warehouse.Backend.API.DTOs.Products;
+
+namespace Novin.Warehouse.Backend.API.DTOs.Inventories
+{
+    public class InventoryDto
+    {
+        public required string Guid { get; set; }
+        public required ProductDto Product { get; set; }
+        public int Quantity { get; set; }
+        public string? Location { get; set; }
+        
+    }
+}
