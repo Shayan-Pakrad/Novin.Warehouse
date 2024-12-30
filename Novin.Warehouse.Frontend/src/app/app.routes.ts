8f6ce4d1efb70ddc9fef92369b75b83204a79bfa
Shayan Pakrad <shaayanpakrad@gmail.com>
2024-12-30 15:40:20 +0330

    Frontend: set up some routes for public and inventory urls

--- Novin.Warehouse.Frontend/src/app/app.routes.ts
+++ Novin.Warehouse.Frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { PublicSharedComponent } from './+public/public-shared/public-shared.component';
+import { InventoryComponent } from './+public/+pages/inventory/inventory.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'public', component: PublicSharedComponent, children: [
+            { path: 'inventory', component: InventoryComponent },
+            { path: '', redirectTo: 'home', pathMatch: 'prefix' }
+        ]
+    },
+    { path: '', redirectTo: 'public', pathMatch: 'full' }
+];
