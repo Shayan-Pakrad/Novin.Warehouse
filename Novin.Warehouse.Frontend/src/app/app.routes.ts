4cd0ca64517e5907b208a6c43b871ce853425f41
Shayan Pakrad <shaayanpakrad@gmail.com>
2024-12-30 15:57:49 +0330

    Frontend: add products routes

--- Novin.Warehouse.Frontend/src/app/app.routes.ts
+++ Novin.Warehouse.Frontend/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { PublicSharedComponent } from './+public/public-shared/public-shared.component';
 import { InventoryComponent } from './+public/+pages/inventory/inventory.component';
 import { TransactionsComponent } from './+public/+pages/transactions/transactions.component';
+import { ProductsComponent } from './+public/+pages/products/products.component';
 
 export const routes: Routes = [
     {
         path: 'public', component: PublicSharedComponent, children: [
             { path: 'inventory', component: InventoryComponent },
             { path: 'transactions', component: TransactionsComponent},
+            { path: 'products', component: ProductsComponent},
             { path: '', redirectTo: 'home', pathMatch: 'prefix' }
         ]
     },
