fa35c218ddfd0689af046d0e822b9bcecb77bdda
Shayan Pakrad <shaayanpakrad@gmail.com>
2025-01-03 20:20:03 +0330

    Frontend: modify product component model

--- Novin.Warehouse.Frontend/src/app/+public/+pages/products/products.component.ts
+++ Novin.Warehouse.Frontend/src/app/+public/+pages/products/products.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../../model/product';
+import { ProductService } from '../../../service/product.service';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) { }
+  
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+    })
+  }
+  
 }
