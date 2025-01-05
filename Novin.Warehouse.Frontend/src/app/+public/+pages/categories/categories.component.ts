4358e8df5ca0c6a9b02f02b8c60eaa3803fbad4b
Shayan Pakrad <shaayanpakrad@gmail.com>
2025-01-06 00:07:08 +0330

    Frontend: modify category component model

--- Novin.Warehouse.Frontend/src/app/+public/+pages/categories/categories.component.ts
+++ Novin.Warehouse.Frontend/src/app/+public/+pages/categories/categories.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../../../model/category';
+import { CategoryService } from '../../../service/category.service';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  categories: Category[] = [];
 
+  constructor(private categoryService: CategoryService) { }
+  
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe((data) => {
+      this.categories = data;
+    })
+  }
 }
