7ed7b829eba43f60cdffdc43428fdacbe4d3a8e3
Shayan Pakrad <shaayanpakrad@gmail.com>
2025-01-06 00:35:49 +0330

    Frontend: modify transaction component model

--- Novin.Warehouse.Frontend/src/app/+public/+pages/transactions/transactions.component.ts
+++ Novin.Warehouse.Frontend/src/app/+public/+pages/transactions/transactions.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Transaction } from '../../../model/transaction';
+import { TransactionService } from '../../../service/transaction.service';
 
 @Component({
   selector: 'app-transactions',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.css'
 })
-export class TransactionsComponent {
+export class TransactionsComponent implements OnInit{
+  transactions: Transaction[] = [];
 
+  constructor(private transactionService: TransactionService) { }
+  
+  ngOnInit(): void {
+    this.transactionService.getTransactions().subscribe((data) => {
+      this.transactions = data;
+    })
+  }
 }
