b56dc820c2584f4f28161ffdd4e508de8956a626
Shayan Pakrad <shaayanpakrad@gmail.com>
2025-01-07 01:14:31 +0330

    Frontend: modify transaction component to add transaction

--- Novin.Warehouse.Frontend/src/app/+public/+pages/transactions/transactions.component.ts
+++ Novin.Warehouse.Frontend/src/app/+public/+pages/transactions/transactions.component.ts
@@ -2,21 +2,54 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Transaction } from '../../../model/transaction';
 import { TransactionService } from '../../../service/transaction.service';
+import { CreateUpdateTransactionDTO } from '../../../model/transaction-add-update.dto';
+import { Product } from '../../../model/product';
+import { ProductService } from '../../../service/product.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-transactions',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.css'
 })
 export class TransactionsComponent implements OnInit{
   transactions: Transaction[] = [];
+  products: Product[] = [];
 
-  constructor(private transactionService: TransactionService) { }
+  newTransaction: CreateUpdateTransactionDTO = {
+    type: true,
+    quantity: 0,
+    productGuid: '',
+  }
+
+  constructor(private transactionService: TransactionService
+    , private productService: ProductService) { }
   
   ngOnInit(): void {
+    this.refreshTransactions();
+    
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+    })
+  }
+
+  refreshTransactions() {
     this.transactionService.getTransactions().subscribe((data) => {
       this.transactions = data;
     })
+  }
+
+  addTransaction() {
+    console.log(this.newTransaction);
+    this.transactionService.addTransaction(this.newTransaction)
+      .subscribe({
+        next: (response) => {
+          console.log('Transaction added successfully:', response);
+          alert('transaction added successfully');
+          this.newTransaction = { productGuid: '', quantity: 0, type: true };
+          this.refreshTransactions();
+        }
+      })
   }
 }
