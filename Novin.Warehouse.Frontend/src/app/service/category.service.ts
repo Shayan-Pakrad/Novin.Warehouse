0503b33b2ee5ff14ce5375b76d227a9ab4bedc74
Shayan Pakrad <shaayanpakrad@gmail.com>
2025-01-06 12:29:53 +0330

    Frontend: modify category service to create category

--- Novin.Warehouse.Frontend/src/app/service/category.service.ts
+++ Novin.Warehouse.Frontend/src/app/service/category.service.ts
@@ -2,16 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Category } from '../model/category';
+import { CreateUpdateCategoryDTO } from '../model/category-add-update.dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
-  private apiUrl = 'http://localhost:5166/api/CategoryApi/list'
+  private apiUrl = 'http://localhost:5166/api/CategoryApi'
 
   constructor(private http: HttpClient) { }
 
   getCategories(): Observable<Category[]> {
-    return this.http.get<Category[]>(this.apiUrl);
+    return this.http.get<Category[]>(this.apiUrl + '/list');
+  }
+
+  addCategory(newCategory: CreateUpdateCategoryDTO): Observable<Category> {
+    return this.http.post<Category>(this.apiUrl + '/add', newCategory);
   }
 }
