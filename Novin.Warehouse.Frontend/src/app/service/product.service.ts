9c9b70a601051e3b6441a1f6be9a8715d2b9d7b1
Shayan Pakrad <shaayanpakrad@gmail.com>
2025-01-06 16:45:02 +0330

    Frontend: modify product service to create product

--- Novin.Warehouse.Frontend/src/app/service/product.service.ts
+++ Novin.Warehouse.Frontend/src/app/service/product.service.ts
@@ -2,16 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../model/product';
+import { CreateUpdateProductDTO } from '../model/product-add-update.dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'http://localhost:5166/api/ProductApi/list'
+  private apiUrl = 'http://localhost:5166/api/ProductApi'
   
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.apiUrl);
+    return this.http.get<Product[]>(`${this.apiUrl}/list`);
+  }
+
+  addProduct(newProduct: CreateUpdateProductDTO): Observable<Product> {
+    return this.http.post<Product>(`${this.apiUrl}/add`, newProduct);
   }
 }
