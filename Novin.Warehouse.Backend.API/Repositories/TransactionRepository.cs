89716cb1c9b5b1b14cb7d277355d79f675dd64d1
Shayan Pakrad <shaayanpakrad@gmail.com>
2025-01-08 00:14:28 +0330

    Backend: modify RemoveAsync function in TransactionRepository

--- Novin.Warehouse.Backend.API/Repositories/TransactionRepository.cs
+++ Novin.Warehouse.Backend.API/Repositories/TransactionRepository.cs
@@ -35,5 +35,28 @@ namespace Novin.Warehouse.Backend.API.Repositories
             await base.AddAsync(transaction);
             return await inventoryRepository.UpdateAsync(inventory);
         }
+
+        public override async Task<int> RemoveAsync(Transaction transaction)
+        {
+            var inventoryRepository = (InventoryRepository)_unitOfWork.GetRepository<Inventory>();
+
+            var inventory = await inventoryRepository.GetByProductIdAsync(transaction.ProductId) ?? throw new Exception("There is no inventory for this product");
+
+            if (transaction.Type == true) // receive
+            {
+                inventory.Quantity -= transaction.Quantity;
+            }
+            else { // dispatch
+                inventory.Quantity += transaction.Quantity;
+            }
+
+            if (inventory.Quantity < 0)
+            {
+                throw new Exception("Don't have much quantity in inventory");
+            }
+
+            await base.RemoveAsync(transaction);
+            return await inventoryRepository.UpdateAsync(inventory);
+        }
     }
 }
