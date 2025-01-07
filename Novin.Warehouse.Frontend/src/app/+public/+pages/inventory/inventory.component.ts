203bcc6f5540713966660e7d07c6d36c2bc05986
Shayan Pakrad <shaayanpakrad@gmail.com>
2025-01-08 01:47:21 +0330

    Frontend: modify inventory component to add transaction

--- Novin.Warehouse.Frontend/src/app/+public/+pages/inventory/inventory.component.ts
+++ Novin.Warehouse.Frontend/src/app/+public/+pages/inventory/inventory.component.ts
@@ -2,22 +2,59 @@ import { Component, OnInit } from '@angular/core';
 import { Inventory } from '../../../model/inventory';
 import { InventoryService } from '../../../service/inventory.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Product } from '../../../model/product';
+import { CreateUpdateTransactionDTO } from '../../../model/transaction-add-update.dto';
+import { TransactionService } from '../../../service/transaction.service';
+import { ProductService } from '../../../service/product.service';
 
 
 @Component({
   selector: 'app-inventory',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.css'
 })
 export class InventoryComponent implements OnInit {
   inventories: Inventory[] = [];
+  products: Product[] = [];
 
-  constructor(private inventoryService: InventoryService) { }
+  newTransaction: CreateUpdateTransactionDTO = {
+    type: true,
+    quantity: 0,
+    productGuid: '',
+  }
+
+  constructor(private inventoryService: InventoryService
+    , private transactionService: TransactionService
+    , private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+    })
+    this.refreshInventories();
+  }
+
+  refreshInventories() {
     this.inventoryService.getInventories().subscribe((data) => {
       this.inventories = data;
     })
+  }
+
+  addTransaction() {
+    this.transactionService.addTransaction(this.newTransaction)
+      .subscribe({
+        next: (response) => {
+          console.log('Transaction added successfully:', response);
+          alert('transaction added successfully');
+          this.newTransaction = { productGuid: '', quantity: 0, type: true };
+          this.refreshInventories();
+        },
+        error: (err) => {
+          console.error('Error adding transaction:', err);
+          alert('Failed to add transaction. Please try again.');
+        }
+      })
   }
 }
