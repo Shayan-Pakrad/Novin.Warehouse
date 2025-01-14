d5981a8860afcecdbbc71b44f8efa6f6acd49d16
Shayan Pakrad <shaayanpakrad@gmail.com>
2025-01-14 19:18:37 +0330

    Frontend: modify public-shared component to show and hide some links based on login status

--- Novin.Warehouse.Frontend/src/app/+public/public-shared/public-shared.component.ts
+++ Novin.Warehouse.Frontend/src/app/+public/public-shared/public-shared.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { LoginResponseDto } from '../../model/login-response.dto';
+import { LoginService } from '../../service/login.service';
+import { User } from '../../model/user.model';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-public-shared',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './public-shared.component.html',
   styleUrl: './public-shared.component.css'
 })
-export class PublicSharedComponent {
+export class PublicSharedComponent implements OnInit, OnDestroy {
+  isLoggedIn: boolean = false;
+  private userSubject: Subscription | null = null;
+
+  constructor(private loginService: LoginService) { }
+
+  ngOnInit() {
+    this.userSubject = this.loginService.user.subscribe((user: User) => {
+      this.isLoggedIn = user ? true : false;
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.userSubject != null)
+    {
+      this.userSubject.unsubscribe();
+    }
+  }
 
 }
