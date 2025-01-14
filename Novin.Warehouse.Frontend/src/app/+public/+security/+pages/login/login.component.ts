07442c112d6d4531fb9a87c78a100e8cfa8bd959
Shayan Pakrad <shaayanpakrad@gmail.com>
2025-01-14 10:18:32 +0330

    Frontend: setup backend calls in login component

--- Novin.Warehouse.Frontend/src/app/+public/+security/+pages/login/login.component.ts
+++ Novin.Warehouse.Frontend/src/app/+public/+security/+pages/login/login.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { LoginRequestDto } from '../../../../model/login-request.dto';
+import { FormsModule } from '@angular/forms';
+import { LoginService } from '../../../../service/login.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginRequest: LoginRequestDto = {
+    username: '',
+    password: ''
+  }
+
+  constructor(private loginService: LoginService) { }
+
+  sendLoginRequest() {
+    this.loginService.login(this.loginRequest).subscribe({
+      next: (response) => {
+        console.log('Logged in successfully: ', response);
+        this.loginRequest = { username: '', password: '' }
+      },
+      error: (err) => {
+        console.log('Error logging', err);
+      }
+    })
+  }
 
 }
