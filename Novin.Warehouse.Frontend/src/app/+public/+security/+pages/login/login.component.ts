82e911aa37792e6e2e588c139a458c0f0631db01
Shayan Pakrad <shaayanpakrad@gmail.com>
2025-01-14 19:16:19 +0330

    Frontend: modify login component to redirect to inventory page when user logs in successfuly

--- Novin.Warehouse.Frontend/src/app/+public/+security/+pages/login/login.component.ts
+++ Novin.Warehouse.Frontend/src/app/+public/+security/+pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { LoginRequestDto } from '../../../../model/login-request.dto';
 import { FormsModule } from '@angular/forms';
 import { LoginService } from '../../../../service/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,13 +16,14 @@ export class LoginComponent {
     password: ''
   }
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private router: Router) { }
 
   sendLoginRequest() {
     this.loginService.login(this.loginRequest).subscribe({
       next: (response) => {
         console.log('Logged in successfully: ', response);
-        this.loginRequest = { username: '', password: '' }
+        this.loginRequest = { username: '', password: '' };
+        this.router.navigate(['/public/inventory']);
       },
       error: (err) => {
         console.log('Error logging', err);
