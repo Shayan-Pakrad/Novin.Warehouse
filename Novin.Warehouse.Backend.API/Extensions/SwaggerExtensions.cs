ffd29a75336bf5d11a07389bd428a976485b3baa
Shayan Pakrad <shaayanpakrad@gmail.com>
2025-02-09 17:13:05 +0330

    Backend: create SwaggerExtension to add my personalized swagger with jwt

---
+++ Novin.Warehouse.Backend.API/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+
+namespace Novin.Warehouse.Backend.API.Extensions
+{
+    public static class SwaggerExtensions
+    {
+        public static void AddSwaggerWithJwt(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter your token in the text input below.\nExample: 'abc123'",
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+        }
+    }
+}
